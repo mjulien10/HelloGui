e58f90617c46eac294d41edb64f1e54fb36fbb4f
mjulien10 <mjmjulien10@gmail.com>
2018-07-25 21:04:42 -0700

    removed extra jlabel

--- src/Hello.java
+++ src/Hello.java
@@ -6,9 +6,8 @@ public class Hello
 	public static void main(String[] args)
 	{
 		MyFrame myframe = new MyFrame("Hello World");
-		JLabel jlabel = new JLabel("Hello World");
+		
 		myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myframe.getContentPane().add(jlabel);
         myframe.setVisible(true);
 
 	}
